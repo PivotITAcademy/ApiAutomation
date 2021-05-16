c902b1aa09c67560dfdce3251586aba767a38af1
ketakipillay <ketakipillay@gmail.com>
2021-05-16 11:48:28 -0400

    Backendless api automation

---
+++ src/test/java/org/example/AppTest.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+
+
+    }
+}
---
+++ src/test/java/org/example/DummyApiTest.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import io.restassured.specification.RequestSpecification;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.*;
+
+public class DummyApiTest {
+
+
+
+}
